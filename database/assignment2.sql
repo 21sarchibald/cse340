c2bbaa4488c48c9c61d710386adb379884134d4f
Sophie Archibald <arc21004@byui.edu>
2025-10-01 17:03:09 -0600

    fixed query 6

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -29,4 +29,5 @@ WHERE classification_name = 'Sport';
 
 -- Query 6
 UPDATE inventory
-SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles');
+SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
