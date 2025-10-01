a20915cfc4d9c21777307a05934a1ae2ff961e48
Sophie Archibald <arc21004@byui.edu>
2025-10-01 16:43:14 -0600

    Finished w03 queries

---
+++ database/assignment2.sql
@@ -0,0 +1,32 @@
+-- Query 1
+INSERT INTO public.account (
+account_firstname, 
+account_lastname, 
+account_email, 
+account_password)
+VALUES (
+'Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n'
+);
+
+-- Query 2
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+-- Query 3
+DELETE FROM account WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+-- Query 4
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make='GM' AND inv_model='Hummer';
+
+-- Query 5
+SELECT inv_make, inv_model
+FROM inventory i JOIN classification c
+ON i.classification_id = c.classification_id
+WHERE classification_name = 'Sport';
+
+-- Query 6
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles');
